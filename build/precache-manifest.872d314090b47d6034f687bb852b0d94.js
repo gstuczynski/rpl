649048c9ff7e56a82799e29d9538a037d42695ed
rzegorz Stuczynski <stuczynski.g@gmail.com>
2023-03-09 13:06:11 +0100

    first commit

---
+++ build/precache-manifest.872d314090b47d6034f687bb852b0d94.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499de6051490dac4b64e19e599ff83ce",
+    "url": "role-playing-life.github.io/index.html"
+  },
+  {
+    "revision": "b96df234e1187bcccb31",
+    "url": "role-playing-life.github.io/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "24bb6d7ff16d892cf335",
+    "url": "role-playing-life.github.io/static/js/2.eb825e91.chunk.js"
+  },
+  {
+    "revision": "b96df234e1187bcccb31",
+    "url": "role-playing-life.github.io/static/js/main.0cdd743e.chunk.js"
+  },
+  {
+    "revision": "8d95b8de3f04a499b1e9",
+    "url": "role-playing-life.github.io/static/js/runtime~main.3f237e22.js"
+  },
+  {
+    "revision": "c446158851c686c1d2f3231bf7015557",
+    "url": "role-playing-life.github.io/static/media/The-Witcher-3-Wild-Hunt-Sword-of-Destiny.c4461588.mp3"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "role-playing-life.github.io/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "role-playing-life.github.io/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "role-playing-life.github.io/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "role-playing-life.github.io/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "role-playing-life.github.io/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "role-playing-life.github.io/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "role-playing-life.github.io/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "af24f45223adce75c1cb080faa9980eb",
+    "url": "role-playing-life.github.io/static/media/under-construction.af24f452.png"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "role-playing-life.github.io/static/media/view-info.415b027d.ico"
+  }
+]);
